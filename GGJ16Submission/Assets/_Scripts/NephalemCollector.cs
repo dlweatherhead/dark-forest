17415efb8ae8ab909721f0c123256ae368394d15
David Weatherhead <dlweatherhead@gmail.com>
2016-01-31 13:03:01 +0200

    Culled some unused Assets

---
+++ GGJ16Submission/Assets/_Scripts/NephalemCollector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NephalemCollector : MonoBehaviour {
+
+	private int nephalemFlamesCollected = 0;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag.Equals("NephalemFlame")) {
+			nephalemFlamesCollected++;
+		}
+	}
+
+	public int flamesCollected() {
+		return nephalemFlamesCollected;
+	}
+}
---
+++ GGJ16Submission/Assets/_Scripts/NephalemSpeakingScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NephalemSpeakingScript : MonoBehaviour {
+
+	public NephalemCollector player;
+	public int flamesRequired;
+
+	public AudioClip ritualCompleteClip;
+	public AudioClip needMoreFlameClip;
+
+	private AudioSource audioSource;
+
+	void Awake() {
+		audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag.Equals("Player")) {
+			if(player.flamesCollected() == flamesRequired) {
+
+				audioSource.clip = ritualCompleteClip;
+				audioSource.Play();
+
+				//AudioSource.PlayClipAtPoint(ritualCompleteClip, gameObject.transform.position);
+
+				StartCoroutine(ResetOnCollision.loadMainScene(ritualCompleteClip.length + 1f));
+			}
+			else {
+				audioSource.clip = needMoreFlameClip;
+				audioSource.Play();
+
+				//AudioSource.PlayClipAtPoint(needMoreFlameClip, other.transform.position);
+			}
+		}
+	}
+
+}
